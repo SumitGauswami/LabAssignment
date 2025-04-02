5d6b4da0a3056e7c4bc066b951956fd896411d61
SumitGauswami <gauswamisumit2901@gmail.com>
2025-04-02 20:57:38 +0530

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mpp.push(nums[i],i)}
+        for(int i=0;i<nums.size();i++){
+            int n=nums[i];
+            int x=target - nums[i];
+           
+            if(mp.find(x)!=mp.end()){
+                return {mp[x],i};
+            }
+            mp[n]=i;
+        }
+        return{-1,-1};
+    }
+};
 
