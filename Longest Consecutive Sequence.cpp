9a1ae9fc41c3a2217d9083b7b39a4efebdd24782
SumitGauswami <gauswamisumit2901@gmail.com>
2025-04-03 22:51:19 +0530

    Update Longest Consecutive Sequence.cpp

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
        sort(nums.begin(),nums.end());
+       if(nums.size()==0) return 0;
        int c=1;
         int mc=0;
         for(int i=1;i<nums.size();i++){
           if(nums[i]==nums[i-1]+1) c++;
           else if(nums[i]==nums[i-1]);
           else {
-          if(mc<c) {
-              mc=c;
-              c=1;
+            mc=max(mc,c);
+            c=1;
           }
         }
-        return mc;
+        return max(mc,c);
     }
 };
