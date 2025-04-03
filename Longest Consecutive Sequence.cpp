a208f68b6375a77bd28555efdf64d841ece88604
SumitGauswami <gauswamisumit2901@gmail.com>
2025-04-03 13:55:45 +0530

    Create Longest Consecutive Sequence.cpp

---
+++ Longest Consecutive Sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       int c=0;
+        for(int i=1;i<nums.size();i++){
+          if(nums[i]==nums[i-1]+1) c++;
+          else if(nums[i]==nums[i-1]);
+          else {
+            break;
+          }
+        }
+        return c+1;
+    }
+};
